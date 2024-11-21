b842f5b7d3298b529c6a06a350b6b3edecaf9dd8
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-21 12:46:41 +0600

    stringify and parse

--- json.js
+++ json.js
@@ -1,4 +1,10 @@
+//stringify: js ar kono value ke json a convert korte use hoy
+// parse: stringify koro kono value je normal js/js object a concert korte use hoy
 const user = { id: 1, name: "rana", age: 23 };
 console.log(user);
+// js obj to json
 const stringified = JSON.stringify(user);
 console.log(stringified);
+//json to normal js object
+const parseFromJson = JSON.parse(stringified);
+console.log(parseFromJson);
