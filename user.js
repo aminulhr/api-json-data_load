203fc4fd61fa0643f8ba4d300f7c25570bb2db18
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-23 01:14:09 +0600

    async awai

--- user.js
+++ user.js
@@ -4,3 +4,9 @@ const loadUser = () => {
     .then((res) => res.json())
     .then((data) => console.log(data));
 };
+
+const loadUser2 = async () => {
+  const user = await fetch("https://jsonplaceholder.org/users");
+  const data = await user.json();
+  console.log(data);
+};
